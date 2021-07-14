4b4a5ee0abb114e34c673b49337770e5cfe8de81
eSuminski <82391246+eSuminski@users.noreply.github.com>
2021-07-14 08:23:05 -0700

    Update Script-7.sql

--- Project1/Script-7.sql
+++ Project1/Script-7.sql
@@ -1,5 +1,5 @@
 
-DROP TABLE employees, managers, employee_login, manager_login, reimbursement;
+DROP TABLE If Exists employees, managers, employee_login, manager_login, reimbursement;
 
 CREATE TABLE employees(
 	employee_id serial PRIMARY KEY,
